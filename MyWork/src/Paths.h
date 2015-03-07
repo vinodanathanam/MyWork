78feafd37dec0991cfbac395b12cdab9180dc4ae
vinod <vinod@vinod-VirtualBox>
2015-03-07 12:10:54 +0000

    Initial Path

---
+++ MyWork/src/Paths.h
@@ -0,0 +1,23 @@
+/*
+ * Paths.h
+ *
+ *  Created on: 7 Mar 2015
+ *      Author: vinod
+ */
+
+#ifndef PATHS_H_
+#define PATHS_H_
+
+
+
+bool findPath(int **grid, int row, int col, int DIM)
+{
+	return false;
+}
+
+void findGridPath()
+{
+
+}
+
+#endif /* PATHS_H_ */
