b9c68565f5e1e8d9a17e89e0c328167f3c61bd06
vinod <vinod@vinod-VirtualBox>
2015-03-07 10:10:29 +0000

    part of queen

--- MyWork/src/MyWork.cpp
+++ MyWork/src/MyWork.cpp
@@ -1,15 +1,23 @@
 //============================================================================
 // Name        : MyWork.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
+#include "dp.h"
+
 using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	vector<long> res(100);
+	res.assign(100, -1);
+	int n = 20;
+
+	printf("Fib of %d is %ld\n", n, fib(n, res));
+
 	return 0;
 }
