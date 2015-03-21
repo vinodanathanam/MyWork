ee66337cfad6c21a552fbe9462ae94a6beeca46d
GIT Admin <vinodanathanam2014@gmail.com>
2015-03-21 12:02:52 +0000

    midway

--- MyWork/src/Tree.h
+++ MyWork/src/Tree.h
@@ -8,6 +8,40 @@
 #ifndef TREE_H_
 #define TREE_H_
 
+struct tNode{
+	int data;
+	tNode* left;
+	tNode* right;
+	tNode* parent;
+	tNode(int d){
+		data = d;
+		left = right = parent = NULL;
+	}
+};
+
+tNode* Insert(tNode* node, int data)
+{
+	if(NULL == node)
+	{
+		tNode t = new tNode(data);
+		return t;
+	}
+	else if (data >= node->data)
+		node->right = Insert(node->right, data);
+	else
+		node->left = Insert(node->left, data);
+
+	return node;
+}
+
+tNode* findMin(tNode* node)
+{
+	tNode* temp = node;
+	while(temp->left){
+		temp = temp->left;
+	}
+	return temp;
+}
 
 
 
