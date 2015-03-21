15eb79ff18974c4b33a981db8f498d8c8bc17b4f
GIT Admin <vinodanathanam2014@gmail.com>
2015-03-21 12:22:05 +0000

    2

--- MyWork/src/Tree.h
+++ MyWork/src/Tree.h
@@ -13,9 +13,8 @@ struct tNode{
 	tNode* left;
 	tNode* right;
 	tNode* parent;
-	tNode(int d){
-		data = d;
-		left = right = parent = NULL;
+	tNode(int d):data(d), left(NULL), right(NULL), parent(NULL){
+
 	}
 };
 
@@ -23,7 +22,7 @@ tNode* Insert(tNode* node, int data)
 {
 	if(NULL == node)
 	{
-		tNode t = new tNode(data);
+		tNode* t = new tNode(data);
 		return t;
 	}
 	else if (data >= node->data)
@@ -32,6 +31,17 @@ tNode* Insert(tNode* node, int data)
 		node->left = Insert(node->left, data);
 
 	return node;
+}
+
+void cleanup(tNode* node)
+{
+	while(NULL != node)
+	{
+		cleanup(node->left);
+		cleanup(node->right);
+		delete node;
+		node = NULL;
+	}
 }
 
 tNode* findMin(tNode* node)
@@ -43,7 +53,55 @@ tNode* findMin(tNode* node)
 	return temp;
 }
 
+void inorder(tNode* node)
+{
+	if(NULL != node)
+	{
+		inorder(node->left);
+		printf("%d ", node->data);
+		inorder(node->right);
+	}
+}
 
+tNode* deleteNode(tNode* node, int d)
+{
+	if(NULL == node)
+		return node;
 
+	if(d > node->data)
+		node->right = deleteNode(node->right, d);
+	else if(d < node->data)
+		node->left = deleteNode(node->left, d);
+	else{
+		//case leaf
+		if(NULL == node->left && NULL == node->right)
+		{
+			delete node;
+			node = NULL;
+		}
+		//case 1 child
+		else if(NULL == node->left)
+		{
+			tNode* temp = node;
+			node = node->right;
+			delete temp;
+			temp = NULL;
+		}
+		else if(NULL == node->right)
+		{
+			tNode* temp = node;
+			node = node->left;
+			delete temp;
+			temp = NULL;
+		}
+		//case 2 children
+		else{
+			tNode* temp = findMin(node->right);
+			node->data = temp->data;
+			node->right = deleteNode(node->right, temp->data);
+		}
+	}
+	return node;
+}
 
 #endif /* TREE_H_ */
